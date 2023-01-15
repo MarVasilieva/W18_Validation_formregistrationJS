440f8ace5b36bb9c325f7c2f05ffd5c6f2b54fb1
MarVasilieva <marina_vasilieva@list.ru>
2023-01-15 13:44:58 +0600

    add tel number

--- script.js
+++ script.js
@@ -2,19 +2,12 @@ let errors = [];
      function checkValidity(input){
         let validity = input.validity;
          if (validity.valueMissing) {
-          errors.push('Поле' + input.placeholder + 'не заполнено ');
+          errors.push('Поле  ' + input.placeholder + ' не заполнено ');
          }
          if (validity.patternMismatch) {
           errors.push('Неверный формат заполнения');
          } 
-         if (validity.rangeOverflow) { let max = getAttributeValue(input,'max');
-            errors.push('Поле' + input.placeholder + 'не заполнено ');
-            errors.push('Максимальное значение не может быть больше чем' + max);
-           } 
-           if (validity.rangeUnderflow) { let min = getAttributeValue(input,'min');
-            errors.push('Поле' + input.placeholder + 'не заполнено ');
-            errors.push('Минимальное значение не может быть больше чем' + min);
-           } 
+         
       }
       function checkAll() {
         errors = [];
